de1d0a1258dc6f7168668d6747a954b3dd106530
hasibur1777 <hasibur.walton@gmail.com>
2022-09-12 12:28:10 +0600

    CORS allow

--- public/index.php
+++ public/index.php
@@ -9,8 +9,8 @@ use Slim\Factory\AppFactory;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization');
-header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
 
 $app = AppFactory::create();
 
